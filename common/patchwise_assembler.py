2e850781dfe7e0dd67ffc1a0cd87024af18c172b
Jan Blechta <blechta@karlin.mff.cuni.cz>
2014-10-07 17:17:22 +0200

    Continue sketching of patchwise assembler.
    TODO: Proper test needed!

--- common/patchwise_assembler.py
+++ common/patchwise_assembler.py
@@ -1,13 +1,14 @@
 from dolfin import *
 import numpy as np
+from petsc4py import PETSc
 
-from common import MatrixView, VectorView
+from common import MatrixView, VectorView, la_index
 
 parameters['graph_coloring_library'] = 'Boost'
 #parameters['graph_coloring_library'] = 'Zoltan'
 
-#mesh = UnitSquareMesh(6, 6)
-mesh = UnitCubeMesh(6, 6, 6)
+mesh = UnitSquareMesh(2, 2)
+#mesh = UnitCubeMesh(6, 6, 6)
 
 comm = mesh.mpi_comm()
 rank = MPI.rank(comm)
@@ -17,6 +18,7 @@ vertex_colors = VertexFunction('size_t', mesh)
 # TODO: These should give same result; which is cheaper?
 vertex_colors.array()[:] = MeshColoring.color(mesh, np.array([0, 1, 0], dtype='uintp'))
 #vertex_colors.array()[:] = MeshColoring.color(mesh, np.array([0, mesh.topology().dim(), 0], dtype='uintp'))
+# TODO: Isn't MPI.max needed here?!
 color_num = int(vertex_colors.array().max() - vertex_colors.array().min() + 1)
 
 max_uintp = int(np.uintp(-1))
@@ -26,9 +28,9 @@ for v in vertices(mesh):
     for c in cells(v):
         cell_partitions[p][c] = p
 
-#plot(vertex_colors)
-#for cp in cell_partitions:
-#    plot(cp)
+plot(vertex_colors)
+for cp in cell_partitions:
+    plot(cp)
 #interactive()
 
 
@@ -40,7 +42,7 @@ for v in vertices(mesh):
 #DistributedMeshTools.number_entities(mesh, 1)
 #print 'facet', rank, f.global_index()
 
-l = 2
+l = 0
 RT = FunctionSpace(mesh, 'Raviart-Thomas', l+1)
 DG = FunctionSpace(mesh, 'Discontinuous Lagrange', l)
 W = RT*DG
@@ -64,26 +66,24 @@ def patch_dim(vertex):
     return num_interior_facets*dofs_per_facet + \
            num_patch_cells*dofs_per_cell # - 1 # factorization to zero mean
 
+num_dofs_with_ghosts = W.dofmap().tabulate_local_to_global_dofs().size # TODO: Is it obtainable cheaply?
 patches_dim = tdim*dofs_per_facet*num_facets + (tdim + 1)*dofs_per_cell*num_cells
 partitions_dim = [sum(patch_dim(v) for v in vertices(mesh) if vertex_colors[v] == p)
                   for p in range(color_num)]
 
 assert sum(patch_dim(v) for v in vertices(mesh)) == patches_dim
-#print rank, partitions_dim, patches_dim
 assert sum(partitions_dim) == patches_dim
 
 patches_dim_offset = MPI.global_offset(comm, patches_dim, True)
-#print rank, patches_dim_offset, patches_dim
 
 # Construct mapping of (rank-local) W dofs to (rank-global) patch-wise problems
-num_dofs_with_ghosts = W.dofmap().tabulate_local_to_global_dofs().size # TODO: Is it obtainable cheaply?
-dof_mapping = [np.empty(num_dofs_with_ghosts, dtype='uintp')
+dof_mapping = [np.empty(num_dofs_with_ghosts, dtype=la_index)
                for p in range(color_num)]
 [dof_mapping[p].fill(-1) for p in range(color_num)]
 dof_counter = patches_dim_offset
 facet_dofs = [W.dofmap().tabulate_entity_dofs(tdim - 1, f)
               for f in range(tdim + 1)]
-cell_dofs = W.dofmap().tabulate_entity_dofs(tdim, 0)
+cell_dofs = W.dofmap().tabulate_entity_dofs(tdim, 0) #[:-1] # factorization to zero mean
 for v in vertices(mesh):
     p = vertex_colors[v]
 
@@ -96,13 +96,63 @@ for v in vertices(mesh):
                 local_dofs += W.dofmap().cell_dofs(c.index())[facet_dofs[j]].tolist()
         local_dofs += W.dofmap().cell_dofs(c.index())[cell_dofs].tolist()
     local_dofs = np.unique(local_dofs)
-    #assert np.unique(local_dofs).tolist() == local_dofs # Does not hold now
 
     # Build global dofs
     num_dofs = patch_dim(v)
     assert num_dofs == len(local_dofs)
-    global_dofs = np.arange(dof_counter, dof_counter + num_dofs, dtype='uintp')
+    global_dofs = np.arange(dof_counter, dof_counter + num_dofs, dtype=la_index)
 
     # Store mapping and increase counter
     dof_mapping[p][local_dofs] = global_dofs
     dof_counter += num_dofs
+
+assert dof_counter == patches_dim + patches_dim_offset
+
+#print rank, dof_mapping
+# TODO: validity of dof_mapping needs to be tested!
+
+A = Matrix() # TODO: needs sparsity pattern!
+b = Vector()
+
+tl_A = TensorLayout(comm, np.array(2*(patches_dim,), dtype='uintp'), 0, 1,
+       np.array(2*((patches_dim_offset, patches_dim_offset + patches_dim), ),
+                dtype='uintp'), False)
+tl_b = TensorLayout(comm, np.array((patches_dim,), dtype='uintp'), 0, 1,
+       np.array(((patches_dim_offset, patches_dim_offset + patches_dim), ),
+                dtype='uintp'), False)
+
+# Empty map should do the same
+#tl.local_to_global_map[0] = np.arange(patches_dim_offset,
+#       patches_dim_offset + patches_dim, dtype='uintp')
+
+# TODO: Sparsity patter missing!
+#A.init(tl_A)
+b.init(tl_b)
+#as_backend_type(A).mat().setOption(
+#    PETSc.Mat.Option.NEW_NONZERO_ALLOCATION_ERR, False)
+# Enforce dropping of negative indices on VecSetValues
+as_backend_type(b).vec().setOption(
+    PETSc.Vec.Option.IGNORE_NEGATIVE_INDICES, True)
+
+#A_patches = [MatrixView(A, W.dim(), W.dim(), dof_mapping[p], dof_mapping[p]) for p in range(color_num)]
+#[assemble(inner(TrialFunction(W), TestFunction(W))*dx, tensor=A_patches[p], add_values=True) for p in range(color_num)]
+b_patches = [VectorView(b, W.dim(), dof_mapping[p]) for p in range(color_num)]
+#[assemble(TestFunctions(W)[1]*dx, tensor=b_patches[p], add_values=True) for p in range(color_num)]
+[assemble(TestFunctions(W)[1]*dx, tensor=b_patches[p], add_values=True) for p in [0]]
+as_backend_type(b).update_ghost_values()
+
+print rank, 'mapping', [dof_mapping[p].size for p in range(color_num)]
+print rank, 'b', b.size(), b.local_size(), b.local_range()
+print rank, 'dofmap', W.dim(), W.dofmap().tabulate_local_to_global_dofs().size, W.dofmap().ownership_range()
+
+w = Function(W)
+#as_backend_type(w.vector()).vec().setOption(
+#    PETSc.Vec.Option.IGNORE_NEGATIVE_INDICES, True)
+#[b1.add_to_vector(w.vector()) for b1 in b_patches]
+for b1 in b_patches:
+    b1.add_to_vector(w.vector())
+    b1.apply('add')
+as_backend_type(w.vector()).update_ghost_values()
+print rank, w.vector().array()
+print rank, w.sub(1, deepcopy=True).vector().array()
+plot(w.sub(1), interactive=True)
