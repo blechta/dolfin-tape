4c343f239a05b91e2c9eaa927b9882ac812af653
Jan Blechta <blechta@karlin.mff.cuni.cz>
2015-01-15 10:38:22 +0100

    Take care of MPI.__TypeDict__ changing to MPI._typedict in future mpi4py release.

--- common/utils.py
+++ common/utils.py
@@ -1,11 +1,15 @@
 from dolfin import as_backend_type, PETScMatrix, MPI, mpi_comm_world, \
                    tic, toc, la_index_dtype
-from mpi4py.MPI import __TypeDict__ as mpi_typedict
+from mpi4py import MPI as MPI4py
 import numpy as np
 
 __all__ = ['la_index_mpitype', 'num_nonzeros']
 
 def la_index_mpitype():
+    try:
+        mpi_typedict = MPI4py._typedict
+    except AttributeError:
+        mpi_typedict = MPI4py.__TypeDict__
     return mpi_typedict[np.dtype(la_index_dtype()).char]
 
 def num_nonzeros(A):
