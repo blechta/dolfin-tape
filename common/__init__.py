4b542c06d2b3fccaa505f3b9f1f9e4a4ccc6406c
Jan Blechta <blechta@karlin.mff.cuni.cz>
2015-02-03 19:18:31 +0100

    Circumvent DOLFIN commit e91b4100.

--- common/__init__.py
+++ common/__init__.py
@@ -1,1 +1,11 @@
+# Avoid PETSc being initialized by DOLFIN, which sets some performance
+# degrading parameters since e91b4100. This assumes that this module
+# is imported before DOLFIN; otherwise the assertion may fail.
+# TODO: Test whether it works!
+from petsc4py import PETSc
+from dolfin import SubSystemsManager
+assert not SubSystemsManager.responsible_petsc()
+
 from flux_reconstructor import FluxReconstructor
+
+__all__ = ['FluxReconstructor']
