baf3f02941a52c73c7be1d032af3f6b30cd893d8
Jan Blechta <blechta@karlin.mff.cuni.cz>
2015-04-05 17:06:37 +0200

    Add machine computation of some manufactured solutions.

--- problems/StokesVortices.py
+++ problems/StokesVortices.py
@@ -2,6 +2,7 @@ from dolfin import *
 
 from common.deviatoric_space import TensorFunctionSpace, deviatoric
 from problems.GeneralizedStokes import GeneralizedStokesProblem
+from problems.exact_solutions import pStokes_vortices
 
 __all__ = ['StokesVortices']
 
@@ -23,28 +24,11 @@ class NewtonianFluid(object):
 class StokesVortices(GeneralizedStokesProblem):
     n = 4 # Number of vortices
     mu = 1.0
-    u_ex = Expression(('+pow(sin(n*pi*x[0]), 2) * sin(2.0*n*pi*x[1])',
-                       '-pow(sin(n*pi*x[1]), 2) * sin(2.0*n*pi*x[0])'),
-                      n=n, degree=6)
-    p_ex = Expression('0.0')
-    s_ex = Expression(
-            (('2.0*mu*n*pi*sin(2.0*n*pi*x[0])*sin(2.0*n*pi*x[1])',
-              '2.0*mu*n*pi*( pow(sin(n*pi*x[0]), 2)*cos(2.0*n*pi*x[1])  '
-              '     - pow(sin(n*pi*x[1]), 2)*cos(2.0*n*pi*x[0]) )'),
-             ('2.0*mu*n*pi*( pow(sin(n*pi*x[0]), 2)*cos(2.0*n*pi*x[1])  '
-              '     - pow(sin(n*pi*x[1]), 2)*cos(2.0*n*pi*x[0]) )',
-              '-2.0*mu*n*pi*sin(2.0*n*pi*x[0])*sin(2.0*n*pi*x[1])')),
-             mu=mu, n=n, degree=6)
-    f = Expression(('+2.0*mu*n*n*pi*pi*( 2.0*pow(sin(n*pi*x[0]), 2) - cos(2.0*n*pi*x[0]) ) * sin(2.0*n*pi*x[1])',
-                    '-2.0*mu*n*n*pi*pi*( 2.0*pow(sin(n*pi*x[1]), 2) - cos(2.0*n*pi*x[1]) ) * sin(2.0*n*pi*x[0])'),
-                   mu=mu, n=n, degree=6)
 
     def __init__(self, N):
         mesh = UnitSquareMesh(N, N, "crossed")
         constitutive_law = NewtonianFluid(self.mu)
+        self.u_ex, self.p_ex, self.s_ex, self.f = \
+            pStokes_vortices(n=self.n, mu=self.mu, r=2, eps=0.0,
+                             degree=6, domain=mesh)
         GeneralizedStokesProblem.__init__(self, mesh, constitutive_law, self.f)
-
-        # Attach domain and element to exact expressions
-        # FIXME: This is a hack
-        for e in [self.u_ex, self.p_ex, self.s_ex]:
-            e._element = e._ufl_element = e.element().reconstruct(domain=mesh)
---
+++ problems/exact_solutions.py
@@ -0,0 +1,44 @@
+from sympy import Symbol, symbols, sin, pi, Matrix, diff, integrate
+from sympy import ccode as sympy_ccode
+
+__all__ = ["pStokes_vortices"]
+
+
+def pStokes_vortices(*args, **kwargs):
+    from dolfin import Expression
+    if kwargs.get('r', 2) == 2:
+        codes = _pStokes_vortices_ccode(r=2)
+    else:
+        codes = _pStokes_vortices_ccode()
+    return (Expression(c, *args, **kwargs) for c in codes)
+
+
+def _pStokes_vortices_ccode(r=None):
+
+    n = Symbol('n', integer=True, positive=True, constant=True)
+    mu = Symbol('mu', positive=True, constant=True)
+    x = symbols('x[0] x[1]')
+    dim = len(x)
+    u = (sin(n*pi*x[0])**2*sin(2*n*pi*x[1]), -sin(n*pi*x[1])**2*sin(2*n*pi*x[0]))
+    p = x[0]**2
+    p = p - integrate(p, (x[0], 0, 1), (x[1], 0, 1))
+
+    L = Matrix(dim, dim, [diff(u[i], x[j]) for i in xrange(dim) for j in xrange(dim)])
+    D = (L+L.T)/2
+    D2 = (D*D.T).trace()
+    eps = Symbol('eps', nonnegative=True, constant=True)
+    if not r:
+        r = Symbol('r', positive=True, constant=True)
+    S = 2*mu*(eps + D2)**(r/2-1)*D
+    divS = tuple(sum(diff(S[i, j], x[j]) for j in xrange(dim)) for i in xrange(dim))
+    gradp = tuple(diff(p, x[i]) for i in xrange(dim))
+    f = tuple(gradp[i] - divS[i] for i in xrange(dim))
+
+    ccode = lambda *args, **kwargs: sympy_ccode(*args, **kwargs).replace('M_PI', 'pi')
+
+    p_code = ccode(p)
+    u_code = tuple(ccode(u[i]) for i in xrange(dim))
+    f_code = tuple(ccode(f[i]) for i in xrange(dim))
+    S_code = tuple(tuple(ccode(S[i, j]) for j in xrange(dim)) for i in xrange(dim))
+
+    return u_code, p_code, S_code, f_code
