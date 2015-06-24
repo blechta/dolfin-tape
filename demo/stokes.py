34482db7c075a099f95a05f85930576662c31832
Jan Blechta <blechta@karlin.mff.cuni.cz>
2015-06-25 00:48:53 +0200

    Polish Stokes demo

--- demo/stokes.py
+++ demo/stokes.py
@@ -1,13 +1,28 @@
+"""This script solves Stokes problem for data with known solution and
+demonstrates efficiency of an error estimate using equilibrated stress
+reconstruction in a spirit similar to
+
+    [A. Hannukainen, R. Stenberg, and M. Vohral\'ik, A unified framework for
+    a posteriori error estimation for the Stokes problem, Numer. Math., 122
+    (2012), pp. 725-769.]
+
+TODO: is there a paper with more closer approach? Hannukainen et al. uses dual
+      mesh approach instead of more recent reconstruction on patches.
+"""
+
 from dolfin import *
 import matplotlib.pyplot as plt
 import numpy as np
 
 from common import FluxReconstructor
+from common.cell_diameter import CellDiameters
+from problems.exact_solutions import pStokes_vortices
 
 
-results = ([], [], [], [], [])
+results = []
 
 for N in [2**i for i in xrange(2, 7)]:
+    # Solve Stokes problem
     mesh = UnitSquareMesh(N, N, 'crossed')
     V = VectorFunctionSpace(mesh, 'CG', 2)
     Q = FunctionSpace(mesh, 'CG', 1)
@@ -15,108 +30,74 @@ for N in [2**i for i in xrange(2, 7)]:
     u, p = TrialFunctions(W)
     v, q = TestFunctions(W)
     a = ( inner(grad(u), grad(v)) - p*div(v) - q*div(u) )*dx
-    u_ex = Expression(('+pow(sin(n*pi*x[0]), 2) * sin(2.0*n*pi*x[1])',
-                       '-pow(sin(n*pi*x[1]), 2) * sin(2.0*n*pi*x[0])'),
-                      n=4.0, degree=6)
-    p_ex = Expression('0.0')
-    f = Expression(('+2.0*n*n*pi*pi*( 2.0*pow(sin(n*pi*x[0]), 2) - cos(2.0*n*pi*x[0]) ) * sin(2.0*n*pi*x[1])',
-                    '-2.0*n*n*pi*pi*( 2.0*pow(sin(n*pi*x[1]), 2) - cos(2.0*n*pi*x[1]) ) * sin(2.0*n*pi*x[0])'),
-                   n=4.0, degree=6)
+    # Retrieve manufactured solution
+    u_ex, p_ex, _, f = pStokes_vortices(n=4, mu=1.0, r=2, eps=0.0,
+                                        degree=6, domain=mesh)
     L = inner(f, v)*dx
     bc_u = DirichletBC(W.sub(0), (0.0, 0.0), lambda x, b: b)
-    bc_p = DirichletBC(W.sub(1), 0.0, "near(x[0], 0.0) && near(x[1], 0.0)", method="pointwise")
+    bc_p = DirichletBC(W.sub(1), 0.0, "near(x[0], 0.0) && near(x[1], 0.0)",
+                       method="pointwise")
     w = Function(W)
     solve(a == L, w, [bc_u, bc_p])
+    u, p = w.split(deepcopy=True)
+    plot(u, title='u (%d x %d)' % (N, N))
 
-    u, p = w.split()
+    # Adjust presure to zero mean
+    p.vector()[:] -= assemble(p*dx)/assemble(Constant(1.0)*dx(mesh))
 
-    print errornorm(u_ex, u, norm_type='H10'), errornorm(Expression('0.0'), p)
-
-    plot(u)
-
-    gdim = mesh.geometry().dim()
-    reconstructor = FluxReconstructor(mesh, 1) # TODO: Is it correct degree?
+    # Reconstruct extra stress q in H(div)
+    reconstructor = FluxReconstructor(mesh, 2) # TODO: Is it correct degree?
     q = []
-    for i in xrange(gdim):
+    tic()
+    for i in range(mesh.geometry().dim()):
         w = reconstructor.reconstruct(-u[i], -f[i] + p.dx(i))
         q.append(w.sub(0, deepcopy=True))
+    t_flux_reconstructor = toc()
+    info_green('Flux reconstruction timing: %g' % t_flux_reconstructor)
     q = as_vector(q)
+    for i in range(mesh.geometry().dim()):
+        plot(q[i], title='q[%d] (%d x %d)' % (i, N, N))
 
-    for i in xrange(gdim):
-        plot(q[i])
+    # Cell size and inf-sup constant
+    h = CellDiameters(mesh)
+    beta = (0.5 - pi**-1)**0.5 # upper bound on inf-sup constant on square
 
-    # TODO: We are missing everywhere pressure error!
+    # Compute actual error using known solution
+    energy_error = ( errornorm(u_ex, u, norm_type='H10')**2
+                   + beta**2*errornorm(p_ex, p)**2 )**0.5
 
-    Au = 0.5*(sqrt(5.0)+1.0)
-    # TODO: h is not generally constant!
-    h = sqrt(2.0)/N
-    # TODO: Obtain Brezzi constant by eigensolver
-    beta = 0.1
-
-    results[0].append((N, V.dim()))
-    energy_error = errornorm(u_ex, u, norm_type='H10') \
-                 + errornorm(p_ex, p)
-
-    # Approximation; works surprisingly well
-    err_est = assemble(inner(grad(u)-q, grad(u)-q)*dx)**0.5 \
-            + h/pi*assemble(inner(div(q)-grad(p)+f, div(q)-grad(p)+f)*dx)**0.5 \
-            + 1.0/beta*assemble(div(u)*div(u)*dx)**0.5
-    #err_est *= Au
-    info_red('Estimator %g, energy_error %g' % (err_est, energy_error))
-    results[1].append((err_est, energy_error))
-
-    # Correct way; slow numpy manipulation is used
+    # Compute error estimate using equilibrated stress reconstruction
     DG0 = FunctionSpace(mesh, 'DG', 0)
     v = TestFunction(DG0)
     err0 = assemble(inner(grad(u)-q, grad(u)-q)*v*dx)
-    err1 = assemble(inner(div(q)-grad(p)+f, div(q)-grad(p)+f)*v*dx)
+    err1 = assemble((h/pi)**2*inner(div(q)-grad(p)+f, div(q)-grad(p)+f)*v*dx)
     err2 = assemble(div(u)*div(u)*v*dx)
-    err0[:] = err0.array()**0.5 + h/pi*err1.array()**0.5
-    err_est = err0.norm('l2') + 1.0/beta*err2.norm('l2')
-    #err_est *= Au
-    info_red('Estimator %g, energy_error %g' % (err_est, energy_error))
-    results[2].append((err_est, energy_error))
+    err0[:] = err0.array()**0.5 + err1.array()**0.5
+    err_est = ( err0.norm('l2')**2 + (1.0/beta*err2.norm('l2'))**2)**0.5
 
-    # Approximation
-    err_est = assemble ( ( inner(grad(u)-q, grad(u)-q)**0.5
-                         + Constant(h/pi)*inner(div(q)-grad(p)+f, div(q)-grad(p)+f)**0.5
-                         )**2*dx
-                         + 1.0/beta*div(u)*div(u)*dx
-                       ) ** 0.5
-    #err_est *= Au
+    # Issue information and store results
     info_red('Estimator %g, energy_error %g' % (err_est, energy_error))
-    results[3].append((err_est, energy_error))
-
-    # Other ways of computing error
-    u_err = errornorm(u_ex, u)
-    #q_ex = Expression(('-m*pi*cos(m*pi*x[0])*sin(n*pi*x[1])',
-    #                   '-n*pi*sin(m*pi*x[0])*cos(n*pi*x[1])'),
-    #                   m=m, n=n, degree=3)
-    #q_err = errornorm(q_ex, q)
-    #info_red('u L2-errornorm %g, q L2-errornorm %g)'%(u_err, q_err))
-    #results[4].append((0.0, q_err))
-    results[4].append((0.0, 1.0))
-    #Q = w.function_space().sub(0).collapse()
-    #info_red('||grad(u)+q||_2 = %g'%norm(project(grad(u)+q, Q)))
-    #info_red('||grad(u)-grad(u_ex)||_2 = %g'%errornorm(q_ex, project(-grad(u), Q)))
+    results.append((N, V.dim(), err_est, energy_error, t_flux_reconstructor))
 
 results = np.array(results, dtype='float')
 
-plt.subplot(4, 1, 1)
-plt.plot(results[0, :, 0], results[1, :, 0], 'o-')
-plt.plot(results[0, :, 0], results[1, :, 1], 'o-')
+# Plot comparison of errors and estimates
+plt.subplot(2, 1, 1)
+plt.plot(results[:, 0], results[:, 2], 'o-', label='estimate')
+plt.plot(results[:, 0], results[:, 3], 'o-', label='error')
+plt.title('Error and its estimate')
+plt.xlabel(r'$1/h$')
+plt.ylabel(r'$(||\mathbf{\nabla u-\nabla u}_h||_2^2'
+           r'+\beta^2||p-p_h||_2^2)^{1/2}$')
 plt.loglog()
-plt.subplot(4, 1, 2)
-plt.plot(results[0, :, 0], results[2, :, 0], 'o-')
-plt.plot(results[0, :, 0], results[2, :, 1], 'o-')
-plt.loglog()
-plt.subplot(4, 1, 3)
-plt.plot(results[0, :, 0], results[3, :, 0], 'o-')
-plt.plot(results[0, :, 0], results[3, :, 1], 'o-')
-plt.loglog()
-plt.subplot(4, 1, 4)
-plt.plot(results[0, :, 0], results[4, :, 0], 'o-')
-plt.plot(results[0, :, 0], results[4, :, 1], 'o-')
+plt.legend()
+
+# Plot timing of flux reconstructions
+plt.subplot(2, 1, 2)
+plt.plot(results[:, 1], results[:, 4], 'o-')
+plt.title('Flux reconstructor timing')
+plt.xlabel('#DOFs')
+plt.ylabel('t [s]')
 plt.loglog()
 
 plt.tight_layout()
