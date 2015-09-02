ec5d6a9db3d73dad00017e50aaeacd672468f8df
Jan Blechta <blechta@karlin.mff.cuni.cz>
2015-09-02 18:16:13 +0200

    Missing copyright headee

--- dolfintape/mesh_diameter.py
+++ dolfintape/mesh_diameter.py
@@ -1,6 +1,24 @@
+# Copyright (C) 2015 Jan Blechta
+#
+# This file is part of dolfin-tape.
+#
+# dolfin-tape is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# dolfin-tape is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with dolfin-tape. If not, see <http://www.gnu.org/licenses/>.
+
 from dolfin import not_working_in_parallel, facets, vertices
 
 __all__ = ['mesh_diameter']
+
 
 def mesh_diameter(mesh):
     # FIXME: Quadratic algorithm is too slow!
