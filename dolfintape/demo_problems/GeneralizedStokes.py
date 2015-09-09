af4d8e51d9d38bba08841253a5623cef55eed70f
Jan Blechta <blechta@karlin.mff.cuni.cz>
2015-09-09 14:34:54 +0200

    Lot of fixes and paper updates in implicit Stokes

--- dolfintape/demo_problems/GeneralizedStokes.py
+++ dolfintape/demo_problems/GeneralizedStokes.py
@@ -22,6 +22,7 @@ import numpy as np
 from dolfintape import FluxReconstructor
 from dolfintape.deviatoric_space import TensorFunctionSpace, deviatoric
 from dolfintape.cell_diameter import CellDiameters
+from dolfintape.poincare import poincare_const
 
 
 class ImplicitStokesFluxReconstructor(FluxReconstructor):
@@ -36,9 +37,8 @@ class ImplicitStokesFluxReconstructor(FluxReconstructor):
         v, phi = TestFunctions(self._W)
         hat = self._hat
         mu, r = float(self._mu), float(self._r)
-        weight = Constant((2.0*mu)**(-1.0/r))
         I_RTN = lambda x: x # Dummy interpolator
-        return ( weight*I_RTN(hat[p]*inner(stress_component, v))
+        return ( Constant(1.0/mu)*I_RTN(hat[p]*inner(stress_component, v))
                  -hat[p]*force_component*phi
                  +inner(grad(hat[p]), stress_component)*phi
                  +residual_component*phi )*self.dp(p)
@@ -48,8 +48,9 @@ class ImplicitStokesFluxReconstructor(FluxReconstructor):
         d, q = TrialFunctions(self._W)
         v, phi = TestFunctions(self._W)
         mu, r = float(self._mu), float(self._r)
-        weight = Constant((2.0*mu)**(-1.0/r))
-        return ( weight*inner(d, v) + inner(q, div(v)) + inner(phi, div(d)) )*self.dp(p)
+        return ( Constant(1.0/mu)*inner(d, v)
+               + inner(q, div(v))
+               + inner(phi, div(d)) )*self.dp(p)
 
 
 class GeneralizedStokesProblem(object):
@@ -69,13 +70,26 @@ class GeneralizedStokesProblem(object):
 
         d = sym(grad(u))
         g = constitutive_law.g()
-        F = ( inner(s, grad(v)) - p*div(v) - q*div(u) + inner(g(s, d), t) - inner(f, v) )*dx
 
-        bc_u = DirichletBC(W.sub(0), (0.0, 0.0), lambda x, b: b)
-        bc_p = DirichletBC(W.sub(1), 0.0, "near(x[0], 0.0) && near(x[1], 0.0)", method="pointwise")
+        # FIXME: get rid of following nasty logic using adaptivity in eps
+        try:
+            g_eps = constitutive_law.g_regularized()
+        except AttributeError:
+            g_eps = None
+            F = ( inner(s, grad(v)) - p*div(v) - q*div(u)
+                + inner(g(s, d), t) - inner(f, v) )*dx
+            F_eps = None
+        else:
+            F = None
+            F_eps = lambda eps: ( inner(s, grad(v)) - p*div(v) - q*div(u)
+                                + inner(g_eps(s, d, eps), t) - inner(f, v) )*dx
+
+        bc_u = DirichletBC(W.sub(0), (0.0, 0.0), "on_boundary")
+        bc_p = DirichletBC(W.sub(1), 0.0, "near(x[0], 0.0) && near(x[1], 0.0)",
+                           method="pointwise")
 
         self._F = F
-        self._J = derivative(self._F, w)
+        self._F_eps = F_eps
         self._bcs = [bc_u, bc_p]
         self._w = w
         self._constitutive_law = constitutive_law
@@ -83,7 +97,15 @@ class GeneralizedStokesProblem(object):
 
 
     def solve(self):
-        solve(self._F == 0, self._w, bcs=self._bcs, J=self._J)
+        if self._F:
+            J = derivative(self._F, self._w)
+            solve(self._F == 0, self._w, bcs=self._bcs, J=J)
+        else:
+            # FIXME: add adaptivity in eps
+            for eps in [10.0**i for i in np.arange(1.0,  -6.0, -0.5)]:
+                F = self._F_eps(eps)
+                J = derivative(F, self._w)
+                solve(F == 0, self._w, bcs=self._bcs, J=J)
         return self._w
 
 
@@ -107,7 +129,6 @@ class GeneralizedStokesProblem(object):
         reconstructor = self.reconstructor()
         f = self._f
         r = self._constitutive_law.r()
-        mu = self._constitutive_law.mu()
 
         u, p, s = w.split()
         s = deviatoric(s)
@@ -127,43 +148,41 @@ class GeneralizedStokesProblem(object):
             q.append(w.sub(0, deepcopy=True))
         q = as_vector(q)
 
-        # Cell diameter
+        # Poincare constant on cells
+        cell_type = mesh.type()
+        C_P = poincare_const(cell_type, r, gdim)
         h = CellDiameters(mesh)
-
-        # Poincare constant on simplex
-        # FIXME: Add Poincare constant for r != 2
-        assert r == 2, "Poincare constant not implemented for r != 2"
-        C_P = 1.0/pi
 
         # Dual Lebesgue exponent; denoted s in the paper
         r1 = r/(r-1)
         # r/2 is dangerous for integer r != 2
         assert isinstance(r, float) and isinstance(r1, float) or r == r1 == 2
-        # FIXME: Wrap float r, r1 by Constant to avoid code generation bloat
 
         # Weights for measuring residual errors for 3 equations
-        alpha = (2.0*float(mu))**(1.0/r)
-        # FIXME: Obtain Brezzi constants by eigensolver
-        beta = Constant(0.1)
-        gamma = Constant(0.1)
+        mu = self._constitutive_law.mu()
+        # FIXME: Inf-sup constant of square; see Costabel for general polygons
+        # https://perso.univ-rennes1.fr/martin.costabel/publis/Co_Mafelap2013_print.pdf
+        beta = (0.5 - 1.0/pi)**0.5
+        # FIXME: Lookup gamma computation in my notes; maybe 1 is correct
+        gamma = 1.0
 
         # Compute estimators
         DG0 = FunctionSpace(mesh, 'DG', 0)
         v = TestFunction(DG0)
-        eta_R_s = assemble(Constant(C_P**r1/alpha**r1)*h*inner(f+div(q), f+div(q))**(r1/2)*v*dx)
-        eta_F_s = assemble(Constant(1.0/alpha**r1)*inner(s-p*I-q, s-p*I-q)**(r1/2)*v*dx)
-        eta_D_r = assemble(Constant(1.0/beta**r)*div(u)**r*v*dx)
-        eta_I_r = assemble(Constant(1.0/gamma**r)*inner(dev(g(s, d)), dev(g(s, d)))**(r/2)*v*dx)
+        eta_O_s = assemble(Constant((C_P/mu)**r1)*h**Constant(r1)*inner(f+div(q), f+div(q))**Constant(r1/2)*v*dx)
+        eta_F_s = assemble(Constant(1.0/mu**r1)*inner(s-p*I-q, s-p*I-q)**Constant(r1/2)*v*dx)
+        eta_D_r = assemble(Constant(1.0/beta**r)*(div(u)*div(u))**Constant(r/2)*v*dx)
+        eta_I_r = assemble(Constant(1.0/gamma**r)*inner(dev(g(s, d)), dev(g(s, d)))**Constant(r/2)*v*dx)
         # TODO: Implement VecPow from PETSc
         # TODO: Avoid drastic numpy manipulations
-        eta_R = eta_R_s.array() ** (1.0/r1)
+        eta_O = eta_O_s.array() ** (1.0/r1)
         eta_F = eta_F_s.array() ** (1.0/r1)
         eta_D = eta_D_r.array() ** (1.0/r)
         eta_I = eta_I_r.array() ** (1.0/r)
         eta_1 = Function(DG0)
         eta_2 = Function(DG0)
         eta_3 = Function(DG0)
-        eta_1.vector()[:] = eta_R + eta_F
+        eta_1.vector()[:] = eta_O + eta_F
         eta_2.vector()[:] = eta_D
         eta_3.vector()[:] = eta_I
         Eta_1 = MPI.sum(comm, np.sum(eta_1.vector().array() ** r1)) ** (1.0/r1)
@@ -180,7 +199,6 @@ class GeneralizedStokesProblem(object):
         w = self._w
         mesh = self._w.function_space().mesh()
         r = self._constitutive_law.r()
-        mu = self._constitutive_law.mu()
 
         u, p, s = w.split()
         s = deviatoric(s)
@@ -189,10 +207,9 @@ class GeneralizedStokesProblem(object):
         r1 = r/(r-1)
         # r/2 is dangerous for integer r != 2
         assert isinstance(r, float) and isinstance(r1, float) or r == r1 == 2
-        # FIXME: Wrap float r, r1 by Constant to avoid code generation bloat
 
         # Weight for residual norm for momentum equation
-        alpha = (2.0*float(mu))**(1.0/r)
+        mu = self._constitutive_law.mu()
 
         I = Identity(mesh.geometry().dim())
         DG0 = FunctionSpace(mesh, 'DG', 0)
@@ -202,10 +219,10 @@ class GeneralizedStokesProblem(object):
         lower = Function(DG0)
         normalization_factors = assemble(inner(grad(u-u_ex),
                                                grad(u-u_ex))
-                                         **(r/2) * v * dx)
-        normalization_factor_global = alpha*normalization_factors.sum()**(1.0/r)
+                                         **Constant(r/2) * v * dx)
+        normalization_factor_global = mu*normalization_factors.sum()**(1.0/r)
         # TODO: Use VecPow, avoid numpy
-        normalization_factors[:] = alpha*normalization_factors.array()**(1.0/r)
+        normalization_factors[:] = mu*normalization_factors.array()**(1.0/r)
         assemble(inner(s-p*I-(s_ex-p_ex*I), grad(u-u_ex))*v*dx,
                  tensor=lower.vector())
         Lower = lower.vector().sum()/normalization_factor_global
@@ -214,11 +231,11 @@ class GeneralizedStokesProblem(object):
 
         # Upper estimate
         upper = Function(DG0)
-        assemble(inner(s-p*I-(s_ex-p_ex*I), s-p*I-(s_ex-p_ex*I))**(r1/2)*v*dx,
+        assemble(inner(s-p*I-(s_ex-p_ex*I), s-p*I-(s_ex-p_ex*I))**Constant(r1/2)*v*dx,
                  tensor=upper.vector())
-        Upper = upper.vector().sum()**(1.0/r1) / alpha
+        Upper = upper.vector().sum()**Constant(1.0/r1) / mu
         # TODO: Use VecPow, avoid numpy
-        upper.vector()[:] = upper.vector().array()**(1.0/r1) / alpha
+        upper.vector()[:] = upper.vector().array()**Constant(1.0/r1) / mu
 
         info_red("Bounds ||R_1||_L, ||R_1||_U: %g, %g" % (Lower, Upper))
 
@@ -236,14 +253,13 @@ class GeneralizedStokesProblem(object):
 
         # Grab constitutive parameters
         r = self._constitutive_law.r()
-        mu = self._constitutive_law.mu()
 
         # Weights for measuring residual errors for 3 equations
-        alpha = (2.0*float(mu))**(1.0/r)
+        mu = self._constitutive_law.mu()
 
         if r != 2:
-            raise NotImplementedError("Momentum resdidual lifting only "
-            "implemented for p == 2.")
+            warning("Momentum resdidual lifting only implemented for r == 2.")
+            return None, None
 
         # Use large degree for lifting residual_1 to H^1_0 to reduce
         # discretization error
@@ -258,7 +274,7 @@ class GeneralizedStokesProblem(object):
         res_1 = Function(lifting_1_space)
         bc_1 = DirichletBC(lifting_1_space, res_1.value_dimension(0)*(0.0,), "on_boundary")
         solve(a_1 == residual_1, res_1, bcs=bc_1)
-        Res_1 = 1.0/alpha*norm(res_1, norm_type="H10")
+        Res_1 = 1.0/mu*norm(res_1, norm_type="H10")
 
         info_red("||R_1||_lifting = %g" % Res_1)
 
